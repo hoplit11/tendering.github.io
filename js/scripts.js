5022a2dc2b257c0195a76ac5505861be4e55cec4
Mauricio <mauricio.ledesma@correounivalle.edu.co>
2020-11-02 22:38:40 -0500

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+
+ $(document).ready(function(){
+  //Carousel-->
+  /*Carousel cycle frecuency*/
+  $("#mycarousel").carousel( { interval: 2000});
+  /**/
+  $("#carouselButton").click(function() {
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) { /*if the span tag has the fa-pause class when is click  do*/
+      $("#mycarousel").carousel('pause'); /*pause the carousel*/
+      $("#carouselButton").children("span").removeClass('fa-pause');/*remove the pause button*//*fliping the pause button to a play button*/
+      $("#carouselButton").children("span").addClass('fa-play'); /*add the play button*//*fliping the pause button to a play button*/
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){ /* else if the span tag has fa-play tag when is click do*/
+      $("#mycarousel").carousel("cycle"); /*cycle the carousel*/
+      $("#carouselButton").children("span").addClass('fa-pause');/*add the class fa-pause(icon) to the span tag */
+    }
+  });
+
+  //ReserveTable Modal
+  /*modal is triggered when the button is clicked*/
+  $("#ReserveTable").click(function(){
+    $("#reserveForm").modal("show");
+  });
+  /*modal is hide when the x is clicked */
+  $("#hideX").click(function(){
+    $("#reserveForm").modal("hide");
+  });
+  /*modal is hide when the cancel button is clicked*/
+  $("#hideCancell").click(function(){
+    $("#reserveForm").modal("hide");
+  });
+
+  //Login Modal
+  /*Login modal Method*/
+  $('#loginModal').click(function(){
+      $('#loginForm').modal('show');
+  });
+  /*Login modal Method close for the X*/
+  $('#closeLog').click(function(){
+      $('#loginForm').modal('hide');
+  });
+  /*Login modal Method close Cancel*/
+  $('#closeLogcancel').click(function(){
+      $('#loginForm').modal('hide');
+  });
+
+  });
